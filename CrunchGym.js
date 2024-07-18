725c84684f524fb52d0105e3e7a32ab0db5eca94
salaheddine mourid <mouridsalah444@gmail.com>
2024-07-18 13:21:21 +0100

    Add files via upload

---
+++ CrunchGym.js
@@ -0,0 +1,155 @@
+ScrollReveal().reveal('.container h2 ', {
+    delay: 100, 
+    duration: 300,
+    origin: 'left', 
+    distance: '80px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.container h4', {
+    delay: 100, 
+    duration: 400,
+    origin: 'left', 
+    distance: '80px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.container h3', {
+    delay: 100, 
+    duration: 500,
+    origin: 'left', 
+    distance: '80px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.container button ', {
+    delay: 100, 
+    duration: 500,
+    origin: 'left', 
+    distance: '80px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('header .img', {
+    delay: 300, 
+    duration: 500,
+    origin: 'right', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+
+// ///////////////
+
+ScrollReveal().reveal('.secend .maincontainer', {
+    delay: 300, 
+    duration: 200,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.secend .S-maincontainer', {
+    delay: 300, 
+    duration: 300,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+
+
+ScrollReveal().reveal('.secend .T-maincontainer', {
+    delay: 300, 
+    duration: 400,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.secend .F-maincontainer', {
+    delay: 300, 
+    duration: 200,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.secend .Fi-maincontainer', {
+    delay: 300, 
+    duration: 300,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.secend .Si-maincontainer', {
+    delay: 300, 
+    duration: 400,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+
+// ////////////
+
+
+ScrollReveal().reveal('.first .schudle img', {
+    delay: 400, 
+    duration: 400,
+    origin: 'left', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.first .schudle .time', {
+    delay: 400, 
+    duration: 400,
+    origin: 'right', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+
+ScrollReveal().reveal('.first .cards-container .one', {
+    delay: 300, 
+    duration: 200,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.first .cards-container .two', {
+    delay: 300, 
+    duration: 400,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.first .cards-container .tree', {
+    delay: 300, 
+    duration: 600,
+    origin: 'bottom', 
+    distance: '60px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+ScrollReveal().reveal('.first .cards-container .four', {
+    delay: 300, 
+    duration: 800,
+    origin: 'bottom', 
+    distance: '100px', 
+    easing: 'ease-in', 
+    reset: false ,
+});
+
+
+
+
+
+
+
+
+
